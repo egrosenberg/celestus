170af868fc79f9928cf3b22f3531317cfb0d77af
Ethan Rosenberg <erosenberg2019@hotmail.com>
2024-12-30 08:24:55 -0800

    fixed an issue where effects from items wouldnt copy their "changes" field

--- module/actors.mjs
+++ module/actors.mjs
@@ -557,6 +557,7 @@ export class CelestusActor extends Actor {
                     disabled: false,
                     type: "status",
                     system: status.system,
+                    changes: status.changes,
                 },
             ]);
         }
